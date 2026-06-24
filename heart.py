58d8b1f09d7e16f7e6abdd6ee354a911a48c2f13
sauravs5271-2526 <saurav.s5271@gmail.com>
2026-06-24 12:27:08 +0530

    added heart code

---
+++ heart.py
@@ -0,0 +1,16 @@
+import math
+from turtle import *
+def hearta(k):
+    return 15 * math.sin(k) **3
+def heartb(k): 
+    return 11.8*math.cos(k)-5*math.cos(2*k)-2*math.cos(3*k)-math.cos(4*k)
+speed(0)
+bgcolor("black")
+for i in range(10000):
+    x=hearta(i)*20
+    y=heartb(i)*20
+    goto(x,y)  
+    for j in range(3):
+       goto(x,y)
+       color("red")
+done()
